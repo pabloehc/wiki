0891c5b4f5a7e5752bf29979c1e1b779d74378c8
Pablo H. Curbelo <curbelo@MacBook-Pro.local>
2020-07-06 00:04:03 -0400

    completed search requirement

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,39 @@
+from django import forms
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
+class SearchForm(forms.Form):
+    query = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
 
 def index(request):
+    entries = util.list_entries()
+    heading = "All Pages"
+
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            # If entry exists, redirect to entry page
+            if util.get_entry(query):
+                return HttpResponseRedirect(reverse('entry', kwargs={'title': query}))
+            
+            # If it does not, check for "similar" entries
+            else:
+                tempEntries = []
+                for entry in entries:
+                    if query.lower() in entry.lower():
+                        tempEntries.append(entry)
+                entries = tempEntries
+                heading = "Similar Pages"
+                     
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries,
+        "form": SearchForm(),
+        "heading": heading
     })
 
 def entry(request, title):
@@ -18,7 +45,7 @@ def entry(request, title):
         
 
     return render(request, "encyclopedia/entry.html", {
-        "title": title,
-        "content": content
+        "title": title.capitalize(),
+        "content": content,
+        "form": SearchForm()
     })
-
