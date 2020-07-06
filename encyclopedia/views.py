a54e4f611cb3569d8cbbd11a457f760f47fb5248
Pablo H. Curbelo <curbelo@MacBook-Pro.local>
2020-07-05 22:11:26 -0400

    entry page requirement done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -6,5 +7,18 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    content = util.get_entry(title)
+
+    if not content:
+        content = f"Sorry! The page for {title} was not found!"
+        title = "Error"
+        
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": content
     })
 
