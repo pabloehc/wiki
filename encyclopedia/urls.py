6ef7c797957ae9d5f557ec0dd447de5af7d47b7a
Pablo H. Curbelo <curbelo@MacBook-Pro.local>
2020-07-07 09:17:06 -0400

    Changed url to wiki/title

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("newpage", views.new_page, name="new_page"),
     path("edit/<title>", views.edit_page, name="edit_page"),
     path("random", views.random, name="random"),
-    path("<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry")
 ]
